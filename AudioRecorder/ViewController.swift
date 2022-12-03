986df15e4f8d3edb2d0c7e17cc8612e332070925
RinStar2003 <mituha133@gmail.com>
2022-12-03 12:52:00 +0200

    - Added button - Added permission request - Added function getDirectory

--- AudioRecorder/ViewController.swift
+++ AudioRecorder/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    
+    @IBOutlet var buttonLabel: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Set up session
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        AVAudioSession.sharedInstance().requestRecordPermission { hasPermission in
+            if hasPermission {
+                print("accepted")
+            } else {
+                print("denined")
+            }
+        }
     }
-
-
+    
+    
+    @IBAction func recordingPressed(_ sender: UIButton) {
+    
+        
+        
+    }
+    
+    
+    // MARK: FUNCTIONS
+    
+    func getDirectory() -> URL {
+        let paths = File
+        
+        return
+    }
+    
+    
 }
 
