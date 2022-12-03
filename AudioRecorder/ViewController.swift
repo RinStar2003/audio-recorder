adb10d30005c0ab68dcb5e90ac36d5664e3e9514
RinStar2003 <mituha133@gmail.com>
2022-12-03 16:45:02 +0200

    - We can record now, but can't see them yet

--- AudioRecorder/ViewController.swift
+++ AudioRecorder/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
     var recordingSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
     
+    var numberOfRecordes = 0
+    
     @IBOutlet var buttonLabel: UIButton!
 
     override func viewDidLoad() {
@@ -32,9 +34,34 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
     
     
     @IBAction func recordingPressed(_ sender: UIButton) {
-    
         
-        
+        // Check if we have an active recorder
+        if audioRecorder == nil {
+            
+            numberOfRecordes += 1
+            let fileName = getDirectory().appendingPathComponent("\(numberOfRecordes).m4a")
+            
+            let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC), AVSampleRateKey: 12000, AVNumberOfChannelsKey: 1, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
+            
+            // Start Audio Recording
+            
+            do {
+                audioRecorder = try AVAudioRecorder(url: fileName, settings: settings)
+                audioRecorder.delegate = self
+                audioRecorder.record()
+                
+                buttonLabel.setTitle("Stop Recording", for: .normal)
+            } catch {
+                displayAlert(title: "Error", message: "Cannot start the recording")
+            }
+        } else {
+            // Stop the recording
+            
+            audioRecorder.stop()
+            audioRecorder = nil
+            
+            buttonLabel.setTitle("Start Recording", for: .normal)
+        }
     }
     
     
