9f380f26c162883d08376c1595c5dcfb44dbb496
RinStar2003 <mituha133@gmail.com>
2022-12-03 13:04:27 +0200

    - function displayAlert()

--- AudioRecorder/ViewController.swift
+++ AudioRecorder/ViewController.swift
@@ -48,6 +48,13 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
         return documentsDirectory
     }
     
+        // Display Alert
+    
+    func displayAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default,handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
     
 }
 
